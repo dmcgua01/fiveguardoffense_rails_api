6f23365ecd1aff1beb47635f3a69de179497c021
Dan McGuane <dmcguane88@gmail.com>
2015-11-09 16:48:15 -0500

    use api subdomain and fix certain tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  post "login", to: "login#login"
-  resources :users, only: %i(create show update destroy)
+  namespace :api, path: '/', constraints: { subdomain: 'api' } do
+    post "login", to: "login#login"
+    resources :users, only: %i(create show update destroy)
+  end
 end
