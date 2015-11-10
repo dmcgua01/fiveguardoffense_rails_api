827a5b31531e0a6682354f0805d58ab39c7e1257
Dan McGuane <dmcguane88@gmail.com>
2015-11-09 19:17:15 -0500

    move api controllers

---
+++ app/controllers/api/login_controller.rb
@@ -0,0 +1,19 @@
+module API
+  class LoginController < ApplicationController
+    respond_to :json
+    skip_before_action :verify_authenticity_token
+
+    def login
+      user = authenticate(login_params)
+      @api_key = user.api_key if user
+
+      render json: { error: "unauthorized" }, status: :not_found if @api_key.nil?
+    end
+
+    private
+
+    def login_params
+      params.require(:password)
+    end
+  end
+end
---
+++ spec/controllers/api/login_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe API::LoginController, type: :controller do
+
+end
